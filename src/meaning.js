0a759403e3c18ce8ca52edb35990cb2ee64b984c
RJP2024 <ruth.jeanphilippe@gmail.com>
2025-01-03 21:03:39 -0500

    Creating synonyms

--- src/meaning.js
+++ src/meaning.js
@@ -9,10 +9,13 @@ export default function meaning(props) {
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
-            <div className="definition">{definition.definition}</div>
-
-            <div className="example">{definition.example}</div>
+           <p>
+            <strong>Definition:</strong> {definition.definition}
+            <br />
+            <strong>Example:</strong>
+            <em>{definition.example}</em>
             <synonyms synonyms={definition.synonyms} />
+          </p>
           </div>
         );
       })}
